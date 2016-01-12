43f9657c1810ea97ea19de0562b0ad8b25f54771
Kathryn Lovell <lovelk2@rpi.edu>
2016-01-12 09:35:22 -0700

    Navigation controller, prior to database changes

---
+++ BB6/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TabTemplate
+//
+//  Created by Kathryn Lovell on 1/12/16.
+//  Copyright © 2016 KLovell. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BB6/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TabTemplate
+//
+//  Created by Kathryn Lovell on 1/12/16.
+//  Copyright © 2016 KLovell. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
